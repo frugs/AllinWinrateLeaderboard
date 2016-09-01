0b74b309afef3880d30207cd4f009f427b83d22d
Hugo Wainwright <wainwrighthugo@gmail.com>
2016-09-02 00:09:04 +0100

    added time zone app

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import re
+import urllib.request
+import json
+import codecs
 from wheezy.template.engine import Engine
 from wheezy.template.ext.core import CoreExtension
 from wheezy.template.loader import FileLoader
 import winrate
 
 
-def wsgi_app(environ, start_response):
+def winrate_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
 
     engine = Engine(loader=FileLoader(['']), extensions=[CoreExtension()])
-    template = engine.get_template('main_template.html')
+    template = engine.get_template('winrate_template.html')
 
     player_wins = winrate.download_current_player_wins()
     leaderboard = winrate.compute_win_rate_leaderboard(player_wins)
@@ -28,3 +32,37 @@ def wsgi_app(environ, start_response):
     })
 
     yield response_body.encode()
+
+
+def timezone_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+
+    geo_response = urllib.request.urlopen("http://freegeoip.net/json/" + environ['REMOTE_ADDR'])
+    geo_info = json.loads(geo_response.read().decode('utf-8'))
+
+    print(geo_info)
+
+    yield geo_info['time_zone'].encode()
+
+
+def not_found_app(environ, start_response):
+    start_response('404 NOT_FOUND', [('Content-type', 'text/plain')])
+    yield "Not found".encode()
+
+
+ROUTES = [
+    (r'^winrate/?$', winrate_app),
+    (r'^timezone/?$', timezone_app),
+    (r'^timezone/.+$', timezone_app)
+]
+
+
+def wsgi_app(environ, start_response):
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    for regex, callback in ROUTES:
+        match = re.search(regex, path)
+        if match is not None:
+            return callback(environ, start_response)
+    return not_found_app(environ, start_response)
